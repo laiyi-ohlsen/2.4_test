33ffd3e59bf6e5bf1c5362e50a73e80a2eb2e7c5
Lai Yi Ohlsen <laiyiohlsen@Lais-MacBook-Air.local>
2025-09-23 23:28:07 -0400

    updated styles

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,10 @@
 module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy("styles");
   return {
     dir: {
       input: ".",     // your source files
-      output: "docs"  // GitHub Pages will serve from this
+      output: "_site"  // GitHub Pages will serve from this
     }
   };
 };
